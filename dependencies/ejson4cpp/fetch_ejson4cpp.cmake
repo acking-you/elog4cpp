576cb6ba397e8dec22fb1d4a924724ba82fc4845
L_B__ <2394626220@qq.com>
2023-04-02 15:14:24 +0800

    rm gitmodules

---
+++ dependencies/ejson4cpp/fetch_ejson4cpp.cmake
@@ -0,0 +1,6 @@
+FetchContent_Declare(
+        ejson4cpp
+        GIT_REPOSITORY https://gitee.com/acking-you/ejson4cpp.git
+        GIT_TAG origin/master
+        GIT_SHALLOW TRUE)
+FetchContent_MakeAvailable(ejson4cpp)
---
+++ dependencies/fmt/fetch_fmt.cmake
@@ -0,0 +1,6 @@
+FetchContent_Declare(
+        fmt
+        GIT_REPOSITORY https://gitee.com/acking-you/fmt.git
+        GIT_TAG origin/master
+        GIT_SHALLOW TRUE)
+FetchContent_MakeAvailable(fmt)
