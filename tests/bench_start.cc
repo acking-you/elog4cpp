826366ec872280e8b3c78bdc009d00b18ffd148b
L_B__ <2394626220@qq.com>
2023-05-26 19:04:01 +0800

    fix:use `FixedBuffer::avail()`

--- tests/bench_start.cc
+++ tests/bench_start.cc
@@ -22,13 +22,13 @@ TEST_CASE("bench start")
    one_thread_sync(1e5, 1);
 //   one_thread_sync_third_part(1e5, 1);
 
-   one_thread_async(1e5, 5);
+//   one_thread_async(1e5, 5);
 //   one_thread_async_third_part(1e5, 5);
 
-   multi_thread_sync(1e4, 10);
+//   multi_thread_sync(1e4, 10);
 //   multi_thread_sync_third_part(1e4, 10);
 
-   multi_thread_async(1e3, 100);
+//   multi_thread_async(1e3, 100);
 //   multi_thread_async_third_part(1e3, 100);
 }
 
