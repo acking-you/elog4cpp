e9d495baa1ce17b157be01464e75a74609cd3425
ackingliu <ackingliu@tencent.com>
2023-11-30 16:02:53 +0800

    adapt msvc

--- include/elog/any.h
+++ include/elog/any.h
@@ -1,6 +1,7 @@
 #pragma once
 
-#if __cplusplus >= 201703l || (_MSC_VER && _MSVC_LANG >= 201703L)
+#if (__cplusplus && __cplusplus >= 201703L) ||                                 \
+  (_MSC_VER && _MSVC_LANG >= 201703L)
 #include <any>
 namespace elog {
 using Any_t = std::any;
